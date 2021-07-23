cb6dc228bd3c3df0ee7f444afcf9e14b7bd5a5da
FR13NDS Ấn <19521187@gmail.com>
2021-07-23 20:14:43 +0700

    update code

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
-  host: "https://appcallvideo.herokuapp.com/",
+  host: "/",
   port: "3001",
 });
 const myVideo = document.createElement("video");
